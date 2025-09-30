3b5ac86e81dddbb86fe91eb622b7b6c7b8b0be5d
Beben-sd <lepron.e2202958@etud.univ-ubs.fr>
2025-09-30 15:28:47 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -2,42 +2,54 @@
 fluidPage(
   theme = shinytheme("flatly"),
   #themeSelector(), # Choix du theme (pour tester les themes)
-
+  
   navbarPage("Application Allocine",
-             
+             sidebarPanel(
+               # Logo Ponant
+               img(src = "logo_ponant.png", height = "90x"),
+               selectInput(inputId = "choix_indicateur", label = "Choix de l'indicateur :",
+                           choices = c("Nombre d'habitants" = "nb_dhabitants",
+                                       "Taux de résidences secondaires" = "taux_de_residendes_secondaires",
+                                       "Prix au mètre carré" = "prix_median_du_bati_au_m2" )),
+               
+               checkboxGroupInput("choix_ile", "Choix d'une ou plusieurs îles : ",  
+                                  choices = unique(data_Ponant$ile) %>% sort()),
+               actionButton("cocher_ile","Cocher toutes les îles"),
+               actionButton("decocher_ile","Décocher toutes les îles"),
+               br(),
+               br(),
+               
+               actionButton("go","Valider")
+               
+               
+               ,width = 3), 
              
              tabPanel("Page principale",  
-                      # Logo Ponant
-                      img(src = "logo_ponant.png", height = "58x"),
                       
-                      # Sidebar with a slider input for number of bins
-                      sidebarLayout(
-                        sidebarPanel(
-                          selectInput(inputId = "choix_indicateur", label = "Choix de l'indicateur :",
-                                      choices = c("Nombre d'habitants", "Taux de résidences secondaires", "Prix au mètre carré")),
-                          
-                          checkboxGroupInput("choix_ile", "Choix d'une ou plusieurs îles : ",  
-                                      choices = unique(data_Ponant$ile) %>% sort()),
-                          actionButton("cocher_ile","Cocher toutes les îles"),
-                          actionButton("decocher_ile","Décocher toutes les îles"),
-                          br(),
-                          br(),
-                          
-                          actionButton("go","Valider")
-                          
-
-                        ),
-                        
-                        # Afficher le graphique d'évolution du nombre de films par an
-                        mainPanel(
-                          tabsetPanel(
-                            tabPanel("Graphique", plotlyOutput("plot_evolution")),
-                            tabPanel("Tableau", tableOutput("table_evolution"))
-                          )
+                      
+                      
+                      # Afficher le graphique d'évolution du nombre de films par an
+                      mainPanel(
+                        tabsetPanel(
+                          tabPanel("Graphique", plotlyOutput("plot_evolution",width = "100%", height = "800px")),
+                          tabPanel("Tableau", DTOutput("table_evolution"))
                         )
                       )
+                       
              ),
-             tabPanel("A propos", # Page secondaire
-                      "Ceci est une application d'exemple basée sur les données", strong("Allociné"))
-  )
+             tabPanel("Comparaison des ", # Page secondaire
+                      
+                      # Afficher le graphique d'évolution du nombre de films par an
+                          
+                      # Afficher le graphique d'évolution du nombre de films par an
+                      mainPanel(
+                        tabsetPanel(
+                          tabPanel("Graphique", plotlyOutput("plot_comparaison",width = "100%", height = "800px"))
+                        )
+                      )
+                      
+                      )
+  
+             
+        )
 )
